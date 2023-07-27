a82b3233005d3fc864d05f4ae53e126174bccb67
Martín Amado Girón <46505170+Requetetin@users.noreply.github.com>
2023-07-26 18:28:46 -0600

    ADD - Drum rythm

---
+++ drumRythms/drumRythm1.py
@@ -0,0 +1,47 @@
+ # drumRythm1.py
+ # Double Kick Offset Drum Pattern
+ 
+from music import *
+
+repetitions = 4
+
+score = Score("One Two Drum Pattern", 180.0)
+
+drumsPart = Part("Drums", 0, 9)
+ 
+bassDrumPhrase = Phrase(0.0)
+snareDrumPhrase = Phrase(0.0)
+hiHatPhrase = Phrase(0.0)
+
+##### create musical data
+ 
+# Kick
+bassPitches   = [BDR] + [REST] * 7 + [BDR] + [REST] * 7
+bassDurations = [EN] * 16
+bassDrumPhrase.addNoteList(bassPitches, bassDurations)
+ 
+# Snare
+snarePitches   = [REST] * 4 + [SNR] + [REST] * 7 + [SNR] + [REST] * 3
+snareDurations = [EN] * 16
+snareDrumPhrase.addNoteList(snarePitches, snareDurations)
+ 
+# hi-hat
+hiHatPitches   = [CHH, REST] * 8
+hiHatDurations = [EN, EN] * 8
+hiHatPhrase.addNoteList(hiHatPitches, hiHatDurations)
+ 
+##### repeat material as needed
+Mod.repeat(bassDrumPhrase, repetitions)
+Mod.repeat(snareDrumPhrase, repetitions)
+Mod.repeat(hiHatPhrase, repetitions)
+ 
+##### combine musical material
+drumsPart.addPhrase(bassDrumPhrase)
+drumsPart.addPhrase(snareDrumPhrase)
+drumsPart.addPhrase(hiHatPhrase)
+score.addPart(drumsPart)
+ 
+##### view and play
+View.sketch(score)
+Play.midi(score)
+Write.midi(score, "drumRythm1.mid")
